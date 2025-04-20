4b368072d53ecb8a31543f1b53ded17f241db40b
Arichy <arichyx@qq.com>
2025-04-20 17:41:36 +0800

    refactor: update .gitignore and clean up grpc.ts

--- frontend/src/grpc.ts
+++ frontend/src/grpc.ts
@@ -1,24 +1,12 @@
-import { createClient, Interceptor, Transport } from '@connectrpc/connect';
+import { createClient, Transport } from '@connectrpc/connect';
 import { createGrpcWebTransport } from '@connectrpc/connect-web';
-import { devtoolsInterceptor } from 'connect-devtools';
 
 import { PersonService } from './gen/person_pb';
 
 const apiUrl = 'http://localhost:8080';
 
-const interceptors: Interceptor[] = [
-  // whatever other interceptors you want
-];
-
-// Only install the devtools interceptor if the gRPC-Web Devtools extension is available
-if ('window' in globalThis && '__CONNECT_WEB_DEVTOOLS__' in globalThis.window) {
-  interceptors.push(devtoolsInterceptor);
-}
-
-// // Now we can use the interceptors in our transport
 export const transport: Transport = createGrpcWebTransport({
   baseUrl: apiUrl,
-  interceptors,
 });
 
 export const personClient = createClient(PersonService, transport);
